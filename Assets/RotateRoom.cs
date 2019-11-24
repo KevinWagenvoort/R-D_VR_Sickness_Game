a73dc7e6f85cf5884aacf409893fb7590e704a9a
Kevin Wagenvoort <k.wagenvoort@devcake.nl>
2019-11-24 23:40:33 +0100

    Unity Oculus Quest Base

---
+++ Assets/RotateRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRoom : MonoBehaviour
+{
+    public Vector3 RotateAmountMax;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 RandomRotate = new Vector3(Random.Range(0, RotateAmountMax.x), Random.Range(0, RotateAmountMax.y), Random.Range(0, RotateAmountMax.z));
+        transform.Rotate(RandomRotate * Time.deltaTime);
+    }
+}
