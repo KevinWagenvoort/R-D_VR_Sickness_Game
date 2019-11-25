527e8faa28214cdf33a724ff286ffcbb049bb96b
Jorik <joriksonneveld@gmail.com>
2019-11-25 11:23:53 +0100

    added capped framereate that goes down every second

---
+++ Assets/FramerateController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FramerateController : MonoBehaviour
+{
+    private float framerate = 72;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = (int)framerate;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        framerate -= Time.deltaTime;
+        Application.targetFrameRate = (int)framerate;
+        Debug.Log(framerate);
+    }
+}
