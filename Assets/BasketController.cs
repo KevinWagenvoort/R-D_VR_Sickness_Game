7c2da012982a97fe5ced5406c3bed7c7315b2afd
Jorik <joriksonneveld@gmail.com>
2019-12-02 13:34:13 +0100

    added a hoop

---
+++ Assets/BasketController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour
+{
+    private int timesScored = 0;
+
+
+    void Update()
+    {
+        Debug.Log(timesScored);
+    }
+
+    public void Score(bool collision, GameObject gameObject)
+    {
+        if (collision)
+        {
+            timesScored++;
+        }
+    }
+}
---
+++ Assets/TagDetectionController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TagDetectionController : MonoBehaviour
+{
+    [Serializable]
+    public class CallbackFunction : UnityEvent<bool, GameObject> { }
+
+    public CallbackFunction callbackFunction;
+
+    public string tagName;
+
+    // Called when the trigger is colliding
+    void OnTriggerEnter(Collider collision)
+    {
+        // Check if the collision is on the right layer
+        if (Detection(collision.gameObject))
+        {
+            callbackFunction.Invoke(true, collision.gameObject);
+        }
+    }
+
+    // Called when the trigger is no longer colliding
+    void OnTriggerExit(Collider collision)
+    {
+        // Check if the collision is on the right layer
+        if (Detection(collision.gameObject))
+        {
+            callbackFunction.Invoke(false, collision.gameObject);
+        }
+    }
+
+    // Called when the trigger is colliding
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision is on the right layer
+        if (Detection(collision.gameObject))
+        {
+            callbackFunction.Invoke(true, collision.gameObject);
+        }
+    }
+
+    // Called when the trigger is no longer colliding
+    void OnCollisionExit(Collision collision)
+    {
+        // Check if the collision is on the right layer
+        if (Detection(collision.gameObject))
+        {
+            callbackFunction.Invoke(false, collision.gameObject);
+        }
+    }
+
+    public bool Detection(GameObject gameObject)
+    {
+        return gameObject.tag.Equals(tagName);
+    }
+}
